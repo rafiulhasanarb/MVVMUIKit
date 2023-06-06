a348354868ab180f6a463a458f9c572ad55b81bd
rafiul hasan <rafiul.hasan@technovicinity.com>
2023-06-06 13:01:41 +0600

    Initial Commit

---
+++ MVVMUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMUIKit
+//
+//  Created by rafiul hasan on 6/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
